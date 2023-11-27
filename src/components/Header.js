bca97539f2bd8beb21265437b0bf35762dc8efbe
suriya1331 <suriyanarayanan1331@gmail.com>
2023-11-27 11:58:42 +0530

    img

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <Box sx={{height:'100px',width:'60%',backgroundColor:'black',position:'sticky',ml:'300px',border:'5px solid yellowgreen'}}>
         <Typography variant='h3' sx={{position:'absolute',left:'300px',top:'10px',color:'white'}}> To Do List </Typography>
+        <img src='Images/1.png' alt='pi' height="200px" width="200px"/>
     </Box>
   )
 }
